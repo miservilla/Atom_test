5fb7c401ca82070e78d1ee93836f5a1e1066bb2e
Michael Servilla <miservilla@gmail.com>
2022-06-09 18:58:34 -0600

    Added more modules

--- Duplicates.py
+++ Duplicates.py
@@ -5,8 +5,6 @@ def duplicates1(L):
     for i in range(n):
         for j in range(n):
             if i !=j and L[i] == L[j]:
-                
-                print(L[i])
                 return True
     return False
 
@@ -20,6 +18,38 @@ def duplicates2(L):
             if L[i] == L[j]:
                 return True
     return False
+
+def duplicates3(L):
+    n = len(L)
+    return any(L[i] == L[j] for i in range(1, n) for j in range(i))
+
+def duplicates4(L):
+    n = len(L)
+    L.sort()
+    for i in range(n - 1):
+        if L[i] == L[i + 1]:
+            return True
+    return False
+
+def duplicates5(L):
+    n = len(L)
+    L.sort()
+    return any(L[i] == L[i + 1] for i in range(n - 1))
+
+def duplicates6(L):
+    s = set()
+    for e in L:
+        if e in s:
+            return True
+        s.add(e)
+    return False
+
+def duplicates7(L):
+    return len(L) != len(set(L))
+
+def duplicates8(L):
+    s = set()
+    return any(e in s or s.add(e) for e in L)
 
 n = 1000
 
@@ -38,4 +68,13 @@ def timetrials(func, n, trials = 10):
     print("average =%10.7f for n = %d" % (totaltime/trials, n))
 
 for n in [50, 100, 200, 400, 800, 1600, 3200]:
-   timetrials(duplicates1, n)
+    print("Quadratic: ", end="")
+    timetrials(duplicates3, n)
+    print("Sorting:", end="")
+    timetrials(duplicates5, n)
+    print("Sets:", end="")
+    timetrials(duplicates7, n)
+    print("---------------------------")
+
+# for n in [50, 100, 200, 400, 800, 1600, 3200]:
+#    timetrials(duplicates8, n)
